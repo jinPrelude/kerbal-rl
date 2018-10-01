c5383e587e03f2fb497aec1e6af782a80559789a
Uijin Jung <jin.Prelude@gmail.com>
2018-10-01 16:26:17 +0900

    Change hyphens to underscore

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name = 'kerbal-rl',
+    name = 'kerbal_rl',
     version = '0.1',
     description = 'Reinforcement learning environment for Kerbal Space Program',
     author = 'Uijin Jung',
