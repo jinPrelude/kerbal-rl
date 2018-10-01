5e5deac08fcc98c78a6aa2914b93770cafabe5f2
Uijin Jung <jin.Prelude@gmail.com>
2018-10-01 16:12:15 +0900

    init

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'kerbal-rl',
+    version = '0.1',
+    description = 'Reinforcement learning environment for Kerbal Space Program',
+    author = 'Uijin Jung',
+    author_email = 'jin.Prelude@gmail.com',
+    long_description=open('README.md').read(),
+    url = 'https://github.com/jinPrelude/kerbal-rl',
+    download_url = 'https://github.com/jinPrelude/kerbal-rl',
+    install_requires = [
+        'numpy',
+        'krpc',
+    ],
+    python_requires = '>=3'
+)
